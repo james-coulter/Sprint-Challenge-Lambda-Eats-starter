69fdcc5d4a2e748a3e1588f46403ac5c28b59761
Christian Coulter <christiancoulter@Christians-MacBook-Air-2.local>
2020-04-24 10:42:42 -0600

    Finished structure and testing. Just need to style for presentation.

--- src/App.js
+++ src/App.js
@@ -1,11 +1,25 @@
-import React from "react";
+import React from 'react'
+import {Route, Link} from "react-router-dom"
+import Home from './Home'
+import Form from './Form'
 
 const App = () => {
   return (
-    <>
-      <h1>Lambda Eats</h1>
-      <p>You can remove this code and create your own header</p>
-    </>
+    <div>
+
+      <div className="links">
+        <Link to='/'>Home</Link>
+        <Link to='/pizza'>Pizza</Link>
+      </div>
+
+      <Route exact path="/">
+      <Home />
+      </Route>
+
+      <Route path="/pizza">
+      <Form />
+      </Route>
+    </div>
   );
 };
 export default App;
